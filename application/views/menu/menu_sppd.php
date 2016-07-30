e409ec8636480dea392b76b4c329493e93b00fad
caphodel <caphodel@gmail.com>
2016-07-30 14:33:38 +0700

    Cleaning

--- application/views/menu/menu_sppd.php
+++
@@ -1,9 +0,0 @@
-HOME;load("sppd_home/index?"+randomString(),"#content");sppdhome
-FPPD;load("sppd_fppd/index?user="+user,"#content");fppd
-FDPD;load("sppd_fdpd/index?user="+user,"#content");fdpd
-REPORT;;sppdreport
-	FPPD;load("sppd_report/fppd?user="+user,"#content");reportfppd
-	FDPD;load("sppd_report/fdpd?user="+user,"#content");reportfdpd
-SYSTEM ADMINISTRATION;;sppdadmin
-	USER MANAGEMENT;load("sppd_settings/index?"+randomString(),"#content");sppduser
-	MASTER;load("sppd_settings/master?"+randomString(),"#content");sppdmaster
--- asset/javascript/jsAjaxFunction.js
+++
@@ -1,9 +0,0 @@
-function generateAjaxTable(args){
-	var params = {
-		ID : "myID",
-		url : "nowhere",
-		interval : 7000
-	}
-	$.extend(params, args);
-}
-
